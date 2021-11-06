1dcc3a98cad4ea1d7a607c6ca7e1d5f597602f27
drox132 <89428086+drox132@users.noreply.github.com>
2021-11-06 00:22:55 -0300

    Update ProductRepository.java

--- src/main/java/com/fran/AppOneOne/Repository/ProductRepository.java
+++ src/main/java/com/fran/AppOneOne/Repository/ProductRepository.java
@@ -29,12 +29,8 @@ public interface ProductRepository extends JpaRepository <Product, Integer> {
     @Query (value= "DELETE FROM PRODUCT WHERE ID = :idusuario ", nativeQuery = true)
     void deleteProductById(@Param("idusuario") int id);
 
-    //tienes que quitar ese / del UPDATE y sabes que tambien le cambie el lugar de esto
-    //lo puse de primero influte?
-
-    // a que haces referenccia? apunta con el mouse m, el orden de la query
-    //no, si la query está bien no pasa nada
-    //ya debe funcionar
+   
+   
 
     @Query (value="UPDATE product SET name= :nameusuario , price = :priceusuario, quantity= :quantityusuario WHERE id = :idusuario ", nativeQuery = true)
     void updateProductById(@Param("idusuario") int id, @Param("nameusuario") String name, @Param("priceusuario") int price, @Param("quantityusuario") int quantity );
@@ -51,14 +47,3 @@ public interface ProductRepository extends JpaRepository <Product, Integer> {
 
 
 
-
-
-//no puedo ayudarte si no me dices que quieres que te ayude
-// no se como hacer la query para insert product debo clocarle los dos puntos y creo que va sin el @
-//correcto
-// y en el arumento lo hao asi @Param("nameVariable") String name, @Param("priceVariable") int price
-//me lo preguntas o me lo dices? te lopregunto
-//si mmm entonces lo voy a hacer pero no me dejes solo que me da miedo <- 💩💩💩💩 jajajja puedes seguir si dale cualquier cosa
-//te escribo si, recuerda revisar lo que ya hiciste en proyectos anteriores y si no entiendes me escribes ok
-//no
-//esta mal?
